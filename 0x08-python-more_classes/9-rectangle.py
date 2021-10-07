9ea153ffae77ce3067c5c4935b17f08651931695
root <e.abiynur@gmail.com>
2021-10-07 03:56:32 -0700

    classes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,7 +10,12 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
-    @staticmethod
+   @classmethod
+    def square(cls, size=0):
+        """returns a new Rectangle instance that is a square w/ h==w==size"""
+        return cls(size, size) 
+
+   @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle based on the area"""
         if type(rect_1) is not Rectangle:
